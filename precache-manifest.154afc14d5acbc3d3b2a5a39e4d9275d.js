3d4fd874d877674e2e600c9c13e3c2b2a7bf86ce
dev-cj <devcj.in@gmail.com>
2020-04-26 13:55:44 +0530

    Updates

---
+++ precache-manifest.154afc14d5acbc3d3b2a5a39e4d9275d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6f55902243f4ae1edcc3cd70181f8c",
+    "url": "/visualize-corona/index.html"
+  },
+  {
+    "revision": "b7fd2ea26b4f770ac562",
+    "url": "/visualize-corona/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "277e537aa68b583e2622",
+    "url": "/visualize-corona/static/css/main.67eb213f.chunk.css"
+  },
+  {
+    "revision": "b7fd2ea26b4f770ac562",
+    "url": "/visualize-corona/static/js/2.4b813bc2.chunk.js"
+  },
+  {
+    "revision": "667aa43c4382ed56f1c571daada90d67",
+    "url": "/visualize-corona/static/js/2.4b813bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "277e537aa68b583e2622",
+    "url": "/visualize-corona/static/js/main.8a0fedda.chunk.js"
+  },
+  {
+    "revision": "b2acf5a73c0896ad5077",
+    "url": "/visualize-corona/static/js/runtime-main.51c3a5cd.js"
+  }
+]);
